9f7d82542c60158d9212b16bf1dd288be1e31d0a
Ololo <vsvktor@gmail.com>
2022-06-06 21:22:52 +0300

    Create favorite menu

--- app/src/main/java/com/example/tmdb/screens/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/tmdb/screens/favorite/FavoriteFragment.kt
@@ -5,14 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tmdb.databinding.FragmentFavoriteBinding
+import com.example.tmdb.databinding.FragmentMainBinding
 
 class FavoriteFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private var mbinding: FragmentFavoriteBinding? = null
+    private val binding get() = mbinding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mbinding = FragmentFavoriteBinding.inflate(
+            layoutInflater,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        inite()
+    }
+
+    private fun inite() {
+        TODO("Not yet implemented")
     }
 }
